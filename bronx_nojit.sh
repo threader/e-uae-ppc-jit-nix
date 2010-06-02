395371b881589e00d12e11dd4d47b944d620e266
Mustafa 'GnoStiC' TUFAN <mtufan@gmail.com>
2010-06-02 08:56:11 +0300

    02.06.2010

---
+++ bronx_nojit.sh
@@ -0,0 +1,4 @@
+./bootstrap.sh
+./configure --with-sdl --with-sdl-gl --with-sdl-gfx --with-sdl-sound --with-caps --with-gayle --enable-drvsnd --enable-amax --enable-cd32 --enable-scsi-device --disable-jit
+make clean
+make
