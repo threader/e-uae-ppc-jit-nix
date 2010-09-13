49540bffe02182aa4b7c7f1efc3c7b6ec4f2cdf6
Mustafa 'GnoStiC' TUFAN <mtufan@gmail.com>
2010-09-13 10:25:09 +0300

    puae 2.3.0

---
+++ build_linux_nojit.sh
@@ -0,0 +1,4 @@
+./bootstrap.sh
+./configure --with-sdl --with-sdl-gl --with-sdl-gfx --with-sdl-sound --with-caps --enable-drvsnd --enable-amax --enable-cd32 --enable-scsi-device --enable-a2091 --enable-gayle --enable-ncr --disable-jit
+make clean
+make
