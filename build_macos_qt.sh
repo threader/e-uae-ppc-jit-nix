0aecfd53855d482d8727942df8ff92a59f82616d
GnoStiC <mtufan@gmail.com>
2010-12-06 17:55:59 +0200

    puae 2.3.1

---
+++ build_macos_qt.sh
@@ -0,0 +1,17 @@
+# P-UAE
+#
+# 2010 Mustafa TUFAN (aka GnoStiC/BRONX)
+#
+#
+#
+base=" --with-sdl --with-sdl-gl --with-sdl-gfx --with-sdl-sound --enable-drvsnd --with-qt "
+cd32=" --enable-cd32 "
+a600=" --enable-gayle "
+scsi=" --enable-scsi-device --enable-ncr --enable-a2091 "
+other=" --with-caps --enable-amax "
+#
+#
+./bootstrap.sh
+./configure $base $cd32 $a600 $scsi $other CFLAGS="-m32" LDFLAGS="-m32" CPPFLAGS="-m32"
+make clean
+make
