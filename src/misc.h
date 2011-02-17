071c59e1c3eaf1199fad746a8b5d5060c4913714
GnoStiC <GnoStiC@GnoStiCs-MacBook-Pro.local>
2011-02-17 22:18:13 +0200

    puae 2.3.1

---
+++ src/misc.h
@@ -0,0 +1,14 @@
+/*
+ * PUAE - The Un*x Amiga Emulator
+ *
+ * Misc
+ *
+ * Copyright 2010-2011 Mustafa TUFAN
+ */
+
+extern int ispressed (int key);
+
+extern int D3D_goodenough (void);
+extern int DirectDraw_CurrentRefreshRate (void);
+extern int DirectDraw_GetVerticalBlankStatus (void);
+
