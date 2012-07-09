80b9ea174b03a62b295e4390a11ff8da84a73d4f
racsdragon <racsdragon@4f586f1d-7e67-4106-9c97-5aa6777136a4>
2012-07-09 07:19:12 +0000

    Added missing includes for MorphOS memory handling.
    (cherry picked from commit fac72432089e7048ea76ef6d26d3df847dc744c2)

--- src/od-amiga/memory.c
+++ src/od-amiga/memory.c
@@ -11,6 +11,12 @@
 
 #ifdef JIT
 
+#ifdef __MORPHOS__
+//We need the memory handling functions for MorphOS
+#include <proto/exec.h>
+#include <exec/system.h>
+#endif
+
 /*
  * Allocate executable memory for JIT cache
  */
