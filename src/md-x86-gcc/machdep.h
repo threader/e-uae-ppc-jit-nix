ce51f99dc8b066ec4110d8becd73f68e0809966d
rofl0r <retnyg@gmx.net>
2014-01-30 23:45:41 +0100

    add a new generic x86 machdep directory
    this merges i386 and x86_64 into a single dir.
    the few differences use #ifdef __x86_64__.

---
+++ src/md-x86-gcc/machdep.h
@@ -0,0 +1,24 @@
+/*
+ * E-UAE - The portable Amiga Emulator
+ *
+ * Processor-specific definitions
+ *
+ * Copyright 2005 Richard Drummond
+ */
+
+#ifndef MACHDEP_MACHDEP_H
+#define MACHDEP_MACHDEP_H
+
+#define MACHDEP_X86
+#ifdef __x86_64__
+#define MACHDEP_NAME    "amd64"
+#else
+#define MACHDEP_NAME    "x86"
+#endif
+
+#define HAVE_MACHDEP_TIMER
+
+typedef uae_s64 frame_time_t;
+#define MAX_FRAME_TIME 9223372036854775807LL
+
+#endif
