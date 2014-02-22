008e87608cfdcd8812702f712d7438cf214274c5
racsdragon <racsdragon@4f586f1d-7e67-4106-9c97-5aa6777136a4>
2014-02-22 06:46:44 +0000

    Mike Blackburn's MacOSX PPC and LinuxPPC fixes: broken cache invalidation code and configure for Darwin.

--- src/include/cacheinvalidate.h
+++ src/include/cacheinvalidate.h
@@ -7,16 +7,14 @@
 
 static inline void inval_icache_range(unsigned long start, unsigned long stop)
 {
-	    unsigned long p;
+	unsigned long p;
 	    
-    stop = stop;
     for (p = start; p < stop;  p++) {
         asm volatile ("dcbst 0,%0" : : "r"(p) : "memory");
     }
     asm volatile ("sync" : : : "memory");
     
-    stop = stop;
-	    for (p = start; p < stop1; p++) {
+	for (p = start; p < stop; p++) {
         asm volatile ("icbi 0,%0" : : "r"(p) : "memory");
     }
     asm volatile ("sync" : : : "memory");
